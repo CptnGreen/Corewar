f5041c46adc1fbac0531e25c70de9c2fc1bd4273
ellaisa <ellaisa@bk.ru>
2020-07-26 19:02:47 +0300

    started worke with print_byte_code

--- includes/corewar.h
+++ includes/corewar.h
@@ -21,5 +21,6 @@ char	*get_magic_header(void);
 char	*chars_to_bytes(char const *str);
 char	*get_zeroes(unsigned int n_zeroes);
 char	*get_byte_code(char const *asm_code);
+int   print_byte_code(char *file_name, t_bot *bot);
 
 #endif
---
+++ test/asm/test_print_byte_code.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "print_byte_code.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_print_byte_code_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement print_byte_code");
+}
