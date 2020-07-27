39b5553af96b94e5fb82f85b7f940ee7ba7d375e
slisandr <slisandr@student.21-school.ru>
2020-07-27 22:28:37 +0300

    Improved get_name_AND_comment() function

--- src/asm/get_name_and_comment.c
+++ src/asm/get_name_and_comment.c
@@ -17,18 +17,16 @@ int		get_name_and_comment(t_bot *bot, char *line, size_t fd)
 		{
 			found_name += 1;
 			get_name_or_comment(bot->name, PROG_NAME_LENGTH, line, fd);
-			if (get_next_line(fd, &line) == 0)
-				break ;
 		}
 		else if (ft_strstr(line, ".comment"))
 		{
 			found_comment += 1;
 			get_name_or_comment(bot->comment, COMMENT_LENGTH, line, fd);
-			if (get_next_line(fd, &line) == 0)
-				break ;
 		}
+		else
+			return (KO);
+		if (found_name > 1 || found_comment > 1 || get_next_line(fd, &line) != 1)
+			return (KO);
 	}
-	if (found_name != 1 || found_comment != 1)
-		return (KO);
 	return (OK);
 }
--- test/asm/test_get_name_and_comment.c
+++ test/asm/test_get_name_and_comment.c
@@ -29,3 +29,20 @@ void test_only_name_multiline(void)
     close(fd);
     remove("test/asm/bot_test.s");
 }
+
+void test_nothing_after_name_and_comment(void)
+{
+    size_t	fd;
+    t_bot	*bot;
+    char	*line;
+
+    fd = open("test/asm/bot_test.s", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    ft_putstr_fd(".name\t\"Sauce\"\n.comment\t\"Lisandra\"\n", fd);
+    lseek(fd, 0, SEEK_SET);
+    get_next_line(fd, &line);
+    bot = init_bot();
+    TEST_ASSERT_EQUAL_INT(KO, get_name_and_comment(bot, line, fd));
+    ft_strdel(&line);
+    close(fd);
+    remove("test/asm/bot_test.s");
+}
