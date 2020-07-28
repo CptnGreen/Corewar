c5a9cd467308c3c0334138ecdf1d8c554d496f5c
eels911 <rigby917@gmail.com>
2020-07-28 18:28:36 +0300

    main

--- src/asm/main.c
+++ src/asm/main.c
@@ -1,6 +1,32 @@
 #include "corewar.h"
 
-int	main(void)
-{
+int	main(int ac, char **av)
+{	
+	t_bot *bot;
+	int i;
+	int err;
+	int fd;
+	
+	i = 0;
+	err = 1;
+	if (!(bot = init_bot()))
+		return(KO);
+	while (++i != ac)
+	{
+		err = check_extension(av[i]);
+		if (err < 0)
+			continue;
+		fd = open(av[i],O_RDONLY);
+		if (fd < 0)
+			return(KO);
+		if ((!(err = get_name_and_comment(bot,fd))))
+			return(KO);
+		if (!(err = get_exec_code()))
+			return(KO);
+		close(fd);
+		if(!(err = print_byte_code(fd,bot)))
+			return(KO);
+	}
+	free(bot);	
 	return (0);
 }
