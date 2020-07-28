b45dde3c35d57fb2e316aff9f9c56a3e1f7f267d
Yesbolat Syilybay <beskaska@gmail.com>
2020-07-28 13:53:31 +0300

    created ceedling module

---
+++ test/asm/test_get_exec_code.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "get_exec_code.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_get_exec_code_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement get_exec_code");
+}
