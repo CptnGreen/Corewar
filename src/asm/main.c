a92e0d5c72896f0ca1e23897543f5699c2243b48
eels911 <rigby917@gmail.com>
2020-07-29 13:22:16 +0300

    delete var err and check norm

--- src/asm/main.c
+++ src/asm/main.c
@@ -1,32 +1,30 @@
 #include "corewar.h"
 
 int	main(int ac, char **av)
-{	
+{
 	t_bot *bot;
 	int i;
-	int err;
 	int fd;
-	
+
 	i = 0;
-	err = 1;
 	if (!(bot = init_bot()))
 		return(KO);
 	while (++i != ac)
 	{
-		err = check_extension(av[i]);
-		if (err < 0)
+
+		if (!(check_extension(av[i])))
 			continue;
 		fd = open(av[i],O_RDONLY);
 		if (fd < 0)
 			return(KO);
-		if ((!(err = get_name_and_comment(bot,fd))))
+		if ((!(get_name_and_comment(bot,fd))))
 			return(KO);
-		if (!(err = get_exec_code()))
+		if (!(get_exec_code()))
 			return(KO);
 		close(fd);
-		if(!(err = print_byte_code(fd,bot)))
+		if(!(print_byte_code(fd,bot)))
 			return(KO);
 	}
-	free(bot);	
+	free(bot);
 	return (0);
 }
