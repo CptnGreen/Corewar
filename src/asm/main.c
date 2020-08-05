95daffe65b2e98a99591caea27bd6a2404a2a9e7
slisandr <slisandr@student.21-school.ru>
2020-08-05 17:52:27 +0300

    Fixed norm errors in main.c

--- src/asm/main.c
+++ src/asm/main.c
@@ -14,27 +14,27 @@
 
 int	main(int ac, char **av)
 {
-	t_bot *bot;
-	int i;
-	int fd;
+	t_bot	*bot;
+	int		i;
+	int		fd;
 
 	i = 0;
-	if (!(bot = init_bot()))
-		return(KO);
+	if (!bot = init_bot())
+		return (KO);
 	while (++i != ac)
 	{
-		if (!(check_extension(av[i])))
-			continue;
+		if (!check_extension(av[i]))
+			continue ;
 		fd = open(av[i], O_RDONLY);
 		if (fd < 0)
-			continue;
-		if ((!(get_name_and_comment(bot,fd))))
-			continue;
-		if (!(get_exec_code()))
-			continue;
+			continue ;
+		if (!get_name_and_comment(bot, fd))
+			continue ;
+		if (!get_exec_code())
+			continue ;
 		close(fd);
-		if(!(print_byte_code(fd,bot)))
-			continue;
+		if (!print_byte_code(fd, bot))
+			continue ;
 	}
 	free(bot);
 	return (0);
