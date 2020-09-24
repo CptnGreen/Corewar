54ddfab303f5764558147f735648736b085ed671
Yesbolat Syilybay <beskaska@gmail.com>
2020-09-24 14:03:46 +0300

    validation code done

--- src/vm/ error.c
+++
@@ -1,8 +0,0 @@
-#include "corewar.h"
-
-void error(char *str,t_list *list)
-{
-    write(1,"Error ",7);
-    write(1,str,ft_strlen(str));
-    write(1,"\n",1);
-}
--- src/vm/validation.c
+++ src/vm/validation.c
@@ -6,11 +6,19 @@
 /*   By: aimelda <aimelda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 15:21:18 by aimelda           #+#    #+#             */
-/*   Updated: 2020/09/23 19:34:11 by aimelda          ###   ########.fr       */
+/*   Updated: 2020/09/24 13:39:45 by aimelda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+#define FLAG_DUMP "-dump"
+#define FLAG_N "-n"
+
+static t_list	*g_unordered_players;
+static int		g_num_players;
+static int		g_cur_order_num;
+static int		g_max_order_num;
 
 static int	check_extension(char *file_name)
 {
@@ -25,95 +33,88 @@ static int	check_extension(char *file_name)
 	return (OK);
 }
 
-static int	free_players(t_list *players, char *err_description)
+static int	error(char *err_description)
 {
-	ft_lstdel(players, free);
+	ft_lstdel(g_unordered_players, free);
 	if (err_description)
 		ft_printf(err_description);
 	return (KO);
 }
 
-void		flag_dump(char *str, t_vm *vm)
+static int	handle_flag(t_vm *vm, char *flag, char *param)
 {
 	int		num; //size_t ???
 	char	*num_as_string;
 
-	num = ft_atoi(str);
+	num = ft_atoi(param);
 	num_as_string = ft_itoa(num);
-	if (!ft_strcmp(str, num_as_string))
-		vm->dump = num;
+	if (!ft_strcmp(flag, FLAG_N))
+	{
+		if (ft_strcmp(param, num_as_string) || (num < 1 && num > MAX_PLAYERS))
+		{
+			free(num_as_string);
+			return (error("error: invalid player number."));
+		}
+		g_cur_order_num = num - 1;
+		if (g_cur_order_num > g_max_order_num)
+			g_max_order_num = g_cur_order_num;
+	}
+	else
+	{
+		if (!ft_strcmp(param, num_as_string))
+			vm->dump = num;
+	}
 	free(num_as_string);
-}
-
-int 		flag_n(char *str, int *cur_number, int *max_number)
-{
-	int	num;
-
-	num = ft_atoi(str);
-	if(num < 1 && num > MAX_PLAYERS)
-		return (KO);
-	*cur_number = num - 1;
-	if (*cur_number > *max_number)
-		*max_number = *cur_number;
 	return (OK);
 }
 
-static int	arrange_players(t_vm *vm, t_list *unordered_players,
-							int num_players, int max_player)
+static int	arrange_players(t_vm *vm)
 {
 	t_list	*tmp;
 	int		i;
 
-	if (max_player >= num_players)
-		return (free_players(unordered_players, 
-		"error: the order number is greater than the total number of players"));
-	if (num_players == 0)
-		return (free_players(unordered_players, USAGE));
+	if (g_max_order_num >= g_num_players)
+		return (error("error: \
+		the order number is greater than the total number of players."));
+	if (g_num_players == 0)
+		return (error(USAGE));
 	i = 0;
-	while (unordered_players)
+	while (g_unordered_players)
 	{
 		while (vm->players[i] != NULL)
 			i++;
-		vm->players[i++] = unordered_players->content;
-		tmp = unordered_players;
-		unordered_players = unordered_players->next;
+		vm->players[i++] = g_unordered_players->content;
+		tmp = g_unordered_players;
+		g_unordered_players = g_unordered_players->next;
 		free(tmp);
 	}
-	return (init_arena(vm, num_players));
+	return (init_arena(vm, g_num_players));
 }
 
 int			validation(t_vm *vm, int argc, char **argv)
 {
-    t_list	*unordered_players;
-    int		col_players;
-    int		cur_order_num;
-    int		max_order_num;
-	int		i;
+	int	i;
 
-	unordered_players = NULL;
-	col_players = 0;
-	cur_order_num = 0;
-	max_order_num = 0;
 	i = 0;
-	while(++i < argc)
+	while (++i < argc)
 		if (check_extension(argv[i]) == OK)
 		{
-			if (++col_players > MAX_PLAYERS)
-				return (free_players(unordered_players)); // "error: too many players."
-			if (bot_processing(vm, &unordered_players, cur_order_num, argv[i]) == KO)
-				return (free_players(unordered_players, NULL));
-			cur_order_num = 0;
+			if (++g_num_players > MAX_PLAYERS)
+				return (error("error: too many players."));
+			if (bot_processing(vm, &g_unordered_players
+			, g_cur_order_num, argv[i]) == KO)
+				return (error(NULL));
+			g_cur_order_num = 0;
 		}
-		else if (cur_order_num > 0)
-			return (free_players(unordered_players)); // "error: invalid sequence of parameters."
-		else if (!ft_strcmp(argv[i], "-dump"))
-			flag_dump(argv[++i], vm);
-		else if (!ft_strcmp(argv[i], "-n"))
+		else if (g_cur_order_num > 0)
+			return (error("error: invalid sequence of parameters."));
+		else if (!ft_strcmp(argv[i], FLAG_N) || !ft_strcmp(argv[i], FLAG_DUMP))
 		{
-			if (flag_n(argv[++i], &cur_order_num, &max_order_num) == KO)
-				return (free_players(unordered_players)); // "error: invalid order number of player"
+			if (handle_flag(vm, argv[i], argv[i + 1]) == KO)
+				return (KO);
+			++i;
 		}
 		else
-			return (free_players(unordered_players)); // "error: illegal option: argv[i]." --- usage
-	return (arrange_players(vm, unordered_players, col_players, max_order_num));
+			return (error("error: illegal option: argv[i]."));
+	return (arrange_players(vm));
 }
