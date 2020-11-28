b2022bc1f032495d9e371b94f99f8248b5d81d6c
CptnGreen <a.krasnoshchekov1992@gmail.com>
2020-11-28 11:51:55 +0300

    Enhance script

--- scripts/check_leaks.sh
+++ scripts/check_leaks.sh
@@ -3,31 +3,37 @@
 #
 # This script is meant to be called via "make memcheck"
 #
-# Put no more than 4 bots you want to test into $bot_dir
+# Put no more than 4 bots you want to test into $bots_dir
+# and run `make memcheck` to do all the functional testing
+#
+# Leaks' reports will be saved into $logs_dir
 #
 
-declare -a bots=()
-declare bot_dir="${PWD}/bots"
-[[ -d $bot_dir ]] || mkdir "${bot_dir}"
-
-bots=($(for bot in $(ls "${bot_dir}"); do echo $bot | sed -r ' s/(.s|.cor)$// '; done | uniq))
-echo -e "List\n===========:"
-echo -e "--> ${bots[0]}"
-echo -e "--> ${bots[1]}"
-echo -e "--> ${bots[2]}"
-echo -e "--> ${bots[3]}"
-echo -e "--> ${bots[4]}"
-echo -e "--> ${bots[5]}"
-echo -e "--> ${bots[6]}"
-echo -e "--> ${bots[7]}"
-echo -e "--> ${bots[8]}"
-echo -e "--> ${bots[9]}"
-echo -e "==========="
-
+# Specify where to store logs
 declare logs_dir="${PWD}/logs/leaks"
-[[ ! -d $logs_dir ]] && mkdir -p "${logs_dir}"
 
+# Specify where to look for bots
+declare bots_dir="${PWD}/bots"
+
+# Must be global
+declare -a bots=()
 declare program=""
+
+get_bots()
+{
+    declare -i n_bots
+
+    [[ -d $bots_dir ]] || { mkdir "${bots_dir}"; echo -e "Dir ${bots_dir} was created; put no more than 4 bots there." >&2; exit 1; }
+    bots=($(for bot in $(ls "${bots_dir}"); do echo $bot | sed -r ' s/(.s|.cor)$// '; done | uniq))
+    n_bots=${#bots[@]}
+    [[ $n_bots -le 4 && $n_bots -ge 1 ]] || { echo -e "Put 1 to 4 bots in the ${bots_dir}." >&2; exit 1; }
+    echo -e "\nFound bots:"
+    echo -e "--> ${bots[0]}"
+    echo -e "--> ${bots[1]}"
+    echo -e "--> ${bots[2]}"
+    echo -e "--> ${bots[3]}"
+    echo -e "-----------"
+}
 
 print_usage()
 {
@@ -44,16 +50,20 @@ log_leaks_using_valgrind()
     if [[ "$program" = "asm" ]]; then
         bot_name="$1"
         printf "\n%s bot: %s\n" "--->" "$bot_name"
-        bot_path="${bot_dir}/${bot_name}.s"
+        bot_path="${bots_dir}/${bot_name}.s"
         log_file="${logs_dir}/${program}_${bot_name}_valgrind.log"
         valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose \
             --log-file="${log_file}" \
             ./"$program" "$bot_path"
-    else
-        log_file="${logs_dir}/${program}_"$(for bot in ${bots[@]}; do printf "%s" ${bot}_; done)"valgrind.log"
+    elif [[ "$program" = "corewar" ]]; then
+        echo -e ""
+        log_file="${logs_dir}/${program}_"$(for bot in $(ls ${bots_dir}/*.cor); do printf "%s" $(basename -s .cor "${bot}")_; done)"valgrind.log"
         valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose \
             --log-file="${log_file}" \
-            ./"$program" $(for bot in "${bots[@]}"; do printf "bots/${bot}.cor "; done)
+            ./"$program" bots/*.cor
+    else
+        echo -e "Internal error: smth wrong with \`program\` var." >&2
+        exit 1
     fi
     printf "%s Leaks report is saved to %s\n" "--->" "$log_file"
 }
@@ -64,10 +74,10 @@ log_leaks_using_leaks()
     printf "\n%s bot: %s\n" "--->" "$bot_name"
     bot_name="$1"
     if [[ "$program" = "asm" ]]; then
-        bot_path="${bot_dir}/${bot_name}.s"
+        bot_path="${bots_dir}/${bot_name}.s"
         iprofiler -leaks -d "${logs_dir}/${program}_${bot_name}_leaks.log" ./"$program" "$bot_path"
     else
-        bot_path="${bot_dir}/${bot_name}.cor"
+        bot_path="${bots_dir}/${bot_name}.cor"
         iprofiler -leaks -d "${logs_dir}/${program}_${bot_name}_leaks.log" ./"$program" "${bots}"
     fi
     printf "%s Leaks report is saved to %s/%s_%s_leaks.log\n" "--->" "$logs_dir" "$program" "$bot_name"
@@ -75,8 +85,8 @@ log_leaks_using_leaks()
 
 run_memcheck()
 {
+    echo -e "\n<<<<< Checking leaks for ${program} >>>>>"
     if [[ "$program" = "asm" ]]; then
-        echo -e "\n*** Checking leaks for ${program} ***"
         for bot in "${bots[@]}"; do
             if command -v valgrind >/dev/null 2>&1; then
                 log_leaks_using_valgrind "${bot}"
@@ -87,8 +97,7 @@ run_memcheck()
                 exit 1
             fi
         done
-    elif [[ "$program" = "vm" ]]; then
-        echo -e "\n*** Checking leaks for ${program} ***"
+    elif [[ "$program" = "corewar" ]]; then
         if command -v valgrind >/dev/null 2>&1; then
             log_leaks_using_valgrind
         elif command -v leaks >/dev/null 2>&1; then
@@ -107,13 +116,21 @@ while getopts "avrh" opt; do
             exit 0
             ;;
         a)
+            [[ ! ${bots} ]] && get_bots
+            [[ ! -d $logs_dir ]] && { mkdir -p "${logs_dir}"; echo -e "Created ${logs_dir} to store logs."; }
             program="asm" && run_memcheck
             ;;
         v)
-            program="vm" && run_memcheck
+            [[ ! ${bots} ]] && get_bots
+            [[ ! -d $logs_dir ]] && { mkdir -p "${logs_dir}"; echo -e "Created ${logs_dir} to store logs."; }
+            program="corewar" && run_memcheck
             ;;
         r)
-            rm -rf "${logs_dir}"
+            if [[ -d $logs_dir ]]; then
+                rm -rf "${logs_dir}" && echo -e "Removed ${logs_dir}."
+            else
+                echo -e "${logs_dir} is absent."
+            fi
             exit 0
             ;;
         *)
@@ -123,7 +140,7 @@ while getopts "avrh" opt; do
     esac
 done
 
-if [[ ! "$program" = "asm" && ! "$program" = "vm" ]]; then
+if [[ ! "$program" = "asm" && ! "$program" = "corewar" ]]; then
     print_usage >&2
     exit 1
 fi
